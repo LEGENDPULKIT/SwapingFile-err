4e6849b665796ca2948231094a9aed7349704164
PULKIT TIWARI <58383745+LEGENDPULKIT@users.noreply.github.com>
2021-02-26 03:04:09 -0800

    Add files via upload

--- SwapingFile.py
+++ SwapingFile.py
@@ -1,18 +1,16 @@
 
 
 def swaping():
-    a=open("file1.txt",'r')
+    a=open(r"D:\PYTHONPROGRAMMING\C98\file1.txt",'r')
     data_a=a.read()
     print(data_a)
 
-    b=open("file2.txt",'r')
+    b=open(r"D:\PYTHONPROGRAMMING\C98\file2.txt",'r')
     data_b=b.read()
     print(data_b)
-    a.write(data_b)
 
-    with open("file1.txt",'w') as a:
+    with open(r"D:\PYTHONPROGRAMMING\C98\file1.txt",'w') as a:
         a.write(data_b)
     print("new",data_a)
-
 
 swaping()
